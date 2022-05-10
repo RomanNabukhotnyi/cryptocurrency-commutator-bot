df89f1a1b195cc93f571b7cc38d0ebf85587fadd
RomanNabukhotnyi <97584054+RomanNabukhotnyi@users.noreply.github.com>
2022-05-10 11:43:53 +0300

    Update controller.ts

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,8 +4,8 @@ import { Command } from './command';
 
 export class Controller {
     static async post(req: Request, res: Response) {
-        const chatId = req.body.callback_query?.message.chat.id ?? req.body.message.chat.id;
-        const text: string = req.body.message?.text.trim() ?? req.body.callback_query.data.trim();
+        const chatId = req.body.callback_query?.message.chat.id ?? req.body.message?.chat.id;
+        const text: string = req.body.message?.text.trim() ?? req.body.callback_query?.data.trim();
         switch (true) {
             case text == '/start':
                 await Command.start(chatId);
@@ -19,14 +19,14 @@ export class Controller {
             case text == '/list_favorite':
                 await Command.listFavorite(chatId);
                 break;
-            case /\/[A-Z]+/.test(text):
-                await Command.coinInfo(chatId, text.replace('/', ''));
-                break;
             case /\/add_to_favorite/.test(text):
-                await Command.addToFavorite(chatId, text.split(/\s+/)[1]);
+                await Command.addToFavorite(chatId, text.split(/\s+/)[1].toLowerCase());
                 break;
             case /\/delete_favorite/.test(text):
-                await Command.deleteFavorite(chatId, text.split(/\s+/)[1]);
+                await Command.deleteFavorite(chatId, text.split(/\s+/)[1].toLowerCase());
+                break;
+            case /\/[A-Za-z]+/.test(text):
+                await Command.coinInfo(chatId, text.replace('/', '').toLowerCase());
                 break;
             default:
                 break;
